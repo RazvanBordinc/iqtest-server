8dc1f425f9da1bac8994c996ae0c1408418af3da
Razvan <razvan@example.com>
2025-05-22 12:58:23 +0300

    migration issue v3

--- Migrations/20250518203428_TestMigration2.cs
+++ Migrations/20250518203428_TestMigration2.cs
@@ -10,6 +10,25 @@ namespace IqTest_server.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // First check if the Country column exists, if not create it
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (
+                    SELECT 1 
+                    FROM sys.columns 
+                    WHERE object_id = OBJECT_ID(N'[dbo].[LeaderboardEntries]') 
+                    AND name = 'Country'
+                )
+                BEGIN
+                    ALTER TABLE [dbo].[LeaderboardEntries] 
+                    ADD [Country] nvarchar(100) NULL;
+                END
+            ");
+
+            // Now update NULL values to empty string and make column NOT NULL
+            migrationBuilder.Sql(@"
+                UPDATE [LeaderboardEntries] SET [Country] = N'' WHERE [Country] IS NULL;
+            ");
+
             migrationBuilder.AlterColumn<string>(
                 name: "Country",
                 table: "LeaderboardEntries",
@@ -26,15 +45,18 @@ namespace IqTest_server.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "Country",
-                table: "LeaderboardEntries",
-                type: "nvarchar(100)",
-                maxLength: 100,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(100)",
-                oldMaxLength: 100);
+            // Check if column exists before trying to alter it
+            migrationBuilder.Sql(@"
+                IF EXISTS (
+                    SELECT 1 
+                    FROM sys.columns 
+                    WHERE object_id = OBJECT_ID(N'[dbo].[LeaderboardEntries]') 
+                    AND name = 'Country'
+                )
+                BEGIN
+                    ALTER TABLE [LeaderboardEntries] ALTER COLUMN [Country] nvarchar(100) NULL;
+                END
+            ");
         }
     }
 }
